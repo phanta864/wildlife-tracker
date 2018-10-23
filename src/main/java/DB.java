6d50ecf3c13e0d9cb74d4246c70d8e68ae12aee3
phanta864 <ephantusmwangi864@gmail.com>
2018-10-23 15:27:29 +0300

    deployed project

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,5 @@
 import org.sql2o.*;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "ephantus", "  ");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-83-49-109.compute-1.amazonaws.com:5432/d6kjhtpla6aqnb", "leaawrynzucvlb", "ecfc62fd135e7e15478f8e9f9fe45e4113e1f9367566033e7ac522270bfe3b97");
 }
